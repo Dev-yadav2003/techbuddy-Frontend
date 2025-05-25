b5a1100ba388c6308cb6a21e715540243a77c049
Dev-yadav2003 <devyadav.lfc.dtu@gmail.com>
2025-05-25 16:39:01 +0530

    Fix env and API setup

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-export const Api_Url = process.env.REACT_APP_API_URL;
+export const Api_Url = import.meta.env.VITE_REACT_APP_API_URL;
+
 fetch(`${Api_Url}/api/users`)
   .then((res) => res.json())
   .then((data) => console.log(data));
