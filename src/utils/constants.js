a7dcb5df2aa0a791b72e75246b03c893fab06994
Dev-yadav2003 <devyadav.lfc.dtu@gmail.com>
2025-05-25 15:12:31 +0530

    udated code

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const Api_Url = "process.env.REACT_APP_API_URL";
+export const Api_Url = process.env.REACT_APP_API_URL;
 fetch(`${Api_Url}/api/users`)
   .then((res) => res.json())
   .then((data) => console.log(data));
