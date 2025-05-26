6554c2ab6cb2c108497d12aaf18c83231b10351b
Dev-yadav2003 <devyadav.lfc.dtu@gmail.com>
2025-05-27 01:29:18 +0530

    update profile issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,22 @@
 import { useSelector } from "react-redux";
 import { Api_Url } from "../utils/constants";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
   const user = useSelector((store) => store.user);
+  const [profileImageUrl, setProfileImageUrl] = useState("");
+
+  useEffect(() => {
+    if (user) {
+      const imageUrl = user.profileImage
+        ? user.profileImage.startsWith("http")
+          ? user.profileImage
+          : `${Api_Url}/uploads/${user.profileImage}`
+        : "/download.png";
+
+      setProfileImageUrl(`${imageUrl}?t=${new Date().getTime()}`);
+    }
+  }, [user]);
 
   return (
     <nav className="bg-blue-900 text-white p-4 flex justify-between items-center">
@@ -11,11 +25,7 @@ const Navbar = () => {
       {user && (
         <div className="flex items-center gap-3">
           <img
-            src={
-              user.profileImageUrl
-                ? `${Api_Url}${user.profileImageUrl}`
-                : "/default-profile.jpg"
-            }
+            src={profileImageUrl || "/default-profile.jpg"}
             alt="Profile"
             className="w-10 h-10 rounded-full object-cover border-2 border-white"
             onError={(e) => {
